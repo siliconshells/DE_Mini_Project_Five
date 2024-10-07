5593425c8de0f9430d41ba639582fa122042f281
Leonard Eshun <leonard@Mac.lan>
2024-10-07 09:49:10 -0400

    updated f string issue in transform

--- my_lib/transform.py
+++ my_lib/transform.py
@@ -6,14 +6,20 @@ import sqlite3
 import csv
 
 
+def create_table(cursor, table, columns, column_attributes):
+    cursor.execute(f"DROP TABLE IF EXISTS {table}")
+    col_attrib_list = [(f"{col} {column_attributes[col]}") for col in columns]
+    cursor.execute(f"CREATE TABLE {table} ({', '.join(col_attrib_list)})")
+
+
 # load the csv file and insert into a new sqlite3 database
 def transform_n_load(
     local_dataset: str,
     database_name: str,
-    new_data_tables : dict,
+    new_data_tables: dict,
     new_lookup_tables: dict,
     column_attributes: dict,
-    column_map: dict
+    column_map: dict,
 ):
     """ "Transforms and Loads data into the local SQLite3 database"""
 
@@ -23,28 +29,27 @@ def transform_n_load(
     conn = sqlite3.connect("data/" + database_name)
 
     c = conn.cursor()
+    # Create tables
     for k, v in new_data_tables.items():
-        c.execute(f"DROP TABLE IF EXISTS {k}")
-        col_attrib_list = [(f"{col} {column_attributes[col]}") for col in v]
-        c.execute(f"CREATE TABLE {k} ({', '.join(col_attrib_list)})")
-        
+        create_table(c, k, v, column_attributes)
+
     for k, v in new_lookup_tables.items():
-        c.execute(f"DROP TABLE IF EXISTS {k}")
-        col_attrib_list = [(f"{col} {column_attributes[col]}") for col in v]
-        c.execute(f"CREATE TABLE {k} ({', '.join(col_attrib_list)})")
-    
+        create_table(c, k, v, column_attributes)
+
     # skip the first row
     next(reader)
     for row in reader:
         for k, v in new_lookup_tables.items():
-            exec_str =  f"select count({v[0]}) from {k} where {v[0]} = {int(row[column_map[v[0]]])}"
+            exec_str = f"select count({v[0]}) from {k} where {v[0]} = {int(row[column_map[v[0]]])}"
             result = c.execute(exec_str).fetchone()[0]
-            if result == 0:     
-                c.execute(f"INSERT INTO {k} ({', '.join(v)}) VALUES ('{"', '".join([(row[column_map[col]]) for col in v])}')")
+            if result == 0:
+                data_values = "', '".join([(row[column_map[col]]) for col in v])
+                c.execute(f"INSERT INTO {k} ({', '.join(v)}) VALUES ('{data_values}')")
                 conn.commit()
 
-        for k, v in new_data_tables.items():          
-            c.execute(f"INSERT INTO {k} ({', '.join(v)}) VALUES ('{"', '".join([(row[column_map[col]]) for col in v])}')")
+        for k, v in new_data_tables.items():
+            data_values = "', '".join([(row[column_map[col]]) for col in v])
+            c.execute(f"INSERT INTO {k} ({', '.join(v)}) VALUES ('{data_values}')")
         conn.commit()
 
     conn.close()
